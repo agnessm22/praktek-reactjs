67a9f23d98611c368ec99a81dc35518337627fb8
Agnes Sri Muliani <justagnes225@outlook.com>
2020-06-15 20:54:52 +0700

    video 1-6

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from './logo.svg';
+import Home from'./Component/Fungsional/Home';
+
+function App() {
+  return (
+    <div className="App">
+    <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Fungsional/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Produk from '../Class/Produk';
+
+const Home = () => {
+    return <div>
+        { 
+        <div>
+        <Produk nama="macbook pro 2020" stock="10" harga="Rp33.000.000"/>
+        <Produk nama="macbook pro 2019" stock="16" harga="Rp39.000.000"/> 
+        <Produk nama="macbook pro 2021" stock="14" harga="Rp53.000.000"/> 
+        <Produk nama="macbook pro 2022" stock="34" harga="Rp73.000.000"/> 
+        <Produk nama="macbook pro 2023" stock="50" harga="Rp43.000.000"/> 
+        <Produk nama="macbook pro 2024" stock="156" harga="Rp23.000.000"/> 
+        </div> }
+      {/*}  <Blog 
+        tanggal="7 Juni 2020"
+        judul="Teknologi Blockchain"
+        summary="Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs."
+        />
+        <Blog 
+        tanggal="8 Juni 2020"
+        judul="Teknologi Internet of Things"
+        summary="Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs."
+        />
+        <Blog 
+        tanggal="9 Juni 2020"
+        judul="Design Pattern Laravel"
+        summary="Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs."
+/> */}
+    </div>
+}
+
+export default Home;
