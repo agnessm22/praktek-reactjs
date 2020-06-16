2090fc8030e5ac57d3d69039bcc51bc6a270df76
Agnes Sri Muliani <justagnes225@outlook.com>
2020-06-16 16:06:10 +0700

    berhasil mengaplikasikan button

--- src/Component/Class/BootstrapComp.jsx
+++ src/Component/Class/BootstrapComp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 class BootstrapComp extends Component {
     constructor(props) {
@@ -13,15 +13,13 @@ class BootstrapComp extends Component {
     render() {
         return (
             <div>
-               <Alert color="primary">
-                   This is a secondary alert - check it out!
-               </Alert>
-               <Alert color="success">
-                   This is a secondary alert - check it out!
-               </Alert>
-               <Alert color="danger">
-                   This is a secondary alert - check it out!
-               </Alert>
+                <Button color="primary">primary</Button>{' '}
+                <Button color="secondary">secondary</Button>{' '}
+                <Button color="success">success</Button>{' '}
+                <Button color="info">info</Button>{' '}
+                <Button color="warning">warning</Button>{' '}
+                <Button color="danger">danger</Button>{' '}
+                <Button color="link">link</Button>
             </div>
         );
     }
