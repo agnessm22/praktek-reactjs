de151707c85af05e8b5e5bba682f275497024454
Agnes Sri Muliani <justagnes225@outlook.com>
2020-06-16 11:17:58 +0700

    berhasil passing data stok pada parent dan child

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Parent from './Component/Class/Parent';
 //import logo from './logo.svg';
-import Home from'./Component/Fungsional/Home';
+//import Home from'./Component/Fungsional/Home';
 
 function App() {
   return (
     <div className="App">
-    <Home/>
+    <Parent/>
     </div>
   );
 }
---
+++ src/Component/Class/Child1.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Child1 extends Component {
+    constructor(props) {
+        super(props)
+       
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Child Component</h3>
+                <p>Stok Produk: {this.props.stock}</p>
+            </div>
+        );
+    }
+}
+
+export default Child1
