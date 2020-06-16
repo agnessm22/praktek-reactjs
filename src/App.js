fdd06a960dce60c18dac412cca6f40df7f8987c2
Agnes Sri Muliani <justagnes225@outlook.com>
2020-06-16 15:36:12 +0700

    berhasil membuat bootstrap komponen yang masih kosong

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Parent from './Component/Class/Parent';
+import BootstrapComp from './Component/Class/BootstrapComp';
+//import Parent from './Component/Class/Parent';
 //import logo from './logo.svg';
 //import Home from'./Component/Fungsional/Home';
 
 function App() {
   return (
     <div className="App">
-    <Parent/>
+    <BootstrapComp/>
     </div>
   );
 }
