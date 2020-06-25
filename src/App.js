bc88b3a3d0a07559a237ed3477b471fff09e4b61
Agnes Sri Muliani <justagnes225@outlook.com>
2020-06-25 23:49:32 +0700

    berhasil membuat homepage dan about menggunakan route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 //import BootstrapComp from './Component/Class/BootstrapComp';
 import NavbarComp from './Component/Fungsional/NavbarComp';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './Component/Fungsional/HomePage';
+import AboutComp from './Component/Fungsional/AboutComp';
 //import Parent from './Component/Class/Parent';
 //import logo from './logo.svg';
-//import Home from'./Component/Fungsional/Home';
+//import Home from'./Component/Fungsional/H
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    <NavbarComp/>
-    </div>
+    <BrowserRouter>
+      <NavbarComp />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutComp} />
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
